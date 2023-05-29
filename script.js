bea3eeb946ca3597a25bc7de8cd07281fd9e1cc1
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-05-29 19:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+<div id="line">
+		
+	</div>
+Shweta Pal7:07 PM
+let selectedElement = document.getElementById("colorSelect");
+	let selectedValue = selectedElement.value;
+	let childrenList = selectedElement.children;
+	let retainedElements = [];
+	 for(let i=0 ;i< childrenList.length ; i++)
+		 {
+			 if(selectedValue===childrenList[i].value)
+			 {
+				 retainedElements.push(selectedValue)
+				 childrenList[i].remove();
+			 }
+				 
+		 }
